b98d0b8f14d36f333772a1ec06ac0119f9871993
AfiyaM <afimr93@gmail.com>
2025-10-19 21:04:54 +0530

    Common Math program - EX02

--- src/ex_02_JavaPrograms/Lab002_Common_Math.java
+++ src/ex_02_JavaPrograms/Lab002_Common_Math.java
@@ -1,13 +1,13 @@
 package ex_02_JavaPrograms;
 
+
 public class Lab002_Common_Math {
 
     public static void main(String[] args) {
         System.out.println(2+2);
-        System.out.println(2*2);
-        System.out.println(2/2);
-        System.out.println(2-2);
-        System.out.println("2+2");
-        System.out.println(3/2); // // println ->Integer part -> No decimal
+        System.out.println(5%2);
+        System.out.println(5/2);
+
+
     }
 }
